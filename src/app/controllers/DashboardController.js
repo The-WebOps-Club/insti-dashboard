0b428ce325d4ead83e0615a23a8b78de71fa1777
Shahidh <shahidhkmuhammed@gmail.com>
2017-03-22 01:12:36 +0530

    Initial commit

---
+++ src/app/controllers/DashboardController.js
@@ -0,0 +1,29 @@
+
+(function(){
+
+  angular
+    .module('app')
+    .controller('DashboardCtrl', [
+      '$scope',
+      DashboardController
+      
+    ]);
+
+  function DashboardController($scope) {
+    $scope.standardItems = [
+      { sizeX: 2, sizeY: 1, row: 0, col: 0 },
+      { sizeX: 2, sizeY: 2, row: 0, col: 2 },
+      { sizeX: 1, sizeY: 1, row: 0, col: 4 },
+      { sizeX: 1, sizeY: 1, row: 0, col: 5 },
+      { sizeX: 2, sizeY: 1, row: 1, col: 0 },
+      { sizeX: 1, sizeY: 1, row: 1, col: 4 },
+      { sizeX: 1, sizeY: 2, row: 1, col: 5 },
+      { sizeX: 1, sizeY: 1, row: 2, col: 0 },
+      { sizeX: 2, sizeY: 1, row: 2, col: 1 },
+      { sizeX: 1, sizeY: 1, row: 2, col: 3 },
+      { sizeX: 1, sizeY: 1, row: 2, col: 4 }
+    ];
+    
+  }
+
+})();

1184335bf3c42b1f8b3c1e1e5ad4b3b14ebdd12e
Shahidh <shahidhkmuhammed@gmail.com>
2017-03-22 17:04:13 +0530

    Disable resizable

--- src/app/components/services/AnnouncementService.js
+++ src/app/components/services/AnnouncementService.js
@@ -4,4 +4,32 @@ var announcement = angular.module('announcementApp', []);
 
 announcement.controller('AnnouncementCtrl', function($scope) {
     $scope.title = 'Announcements';
+
+    $scope.announcements = [
+        {
+            title: 'Sample',
+            description: 'Lorel ipsum asdasdjfiadasd',
+            done: false 
+        },
+        {
+            title: 'Sample',
+            description: 'Lorel ipsum asdasdjfiadasd',
+            done: false 
+        },
+        {
+            title: 'Sample',
+            description: 'Lorel ipsum asdasdjfiadasd',
+            done: false 
+        },
+        {
+            title: 'Sample',
+            description: 'Lorel ipsum asdasdjfiadasd',
+            done: false 
+        },
+        {
+            title: 'Sample',
+            description: 'Lorel ipsum asdasdjfiadasdLorel ipsum asdasdjfiadasdLorel ipsum asdasdjfiadasdLorel ipsum asdasdjfiadasdLorel ipsum asdasdjfiadasdLorel ipsum asdasdjfiadasd',
+            done: false 
+        }
+    ];
 });
--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -10,10 +10,24 @@
     ]);
 
   function DashboardController($scope) {
-    $scope.announcement = {
-        sizeX: 2,
-        sizeY: 2
-    };
+    $scope.gridsterOpts = {
+			margins: [10, 10],
+			columns: 6,
+			draggable: {
+				handle: 'md-toolbar'
+			},
+			defaultSizeX: 2, // the default width of a gridster item, if not specifed
+			defaultSizeY: 1, // the default height of a gridster item, if not specified
+			minSizeX: 1, // minimum column width of an item
+			maxSizeX: null, // maximum column width of an item
+			minSizeY: 1, // minumum row height of an item
+			maxSizeY: null, // maximum row height of an item
+			resizable: {
+				enabled: false
+			} 
+		};
+
+
     
     $scope.weatherGrid = {
       // maxSizeX: 4,

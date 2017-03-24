a1fc909bc2312ad9e27c2159013cad37cce1efda
Shahidh <shahidhkmuhammed@gmail.com>
2017-03-25 01:05:28 +0530

    Add home controller

---
+++ src/app/pages/home/HomeCtrl.js
@@ -0,0 +1,16 @@
+/**
+ * @author shahidhk
+ * created on 24.03.2017
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.home')
+      .controller('HomeCtrl', HomeCtrl);
+
+  /** @ngInject */
+  function HomeCtrl() {
+
+  }
+
+})();
